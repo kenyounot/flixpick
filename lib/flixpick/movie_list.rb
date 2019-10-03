8cf1b6cee18d038df1c20f4e554969e9674b1bb8
Kenneth Spatta <kenyounot@gmail.com>
2019-10-03 11:12:51 -0400

    able to return list of genre names

--- lib/flixpick/movie_list.rb
+++ lib/flixpick/movie_list.rb
@@ -4,7 +4,10 @@ require_relative 'movie.rb'
 require_relative 'genre.rb'
 
 class MovieList
-    
+    def self.list_of_genre_names
+        Genre.all.collect {|g| g.name}
+    end
+
     def self.create_movie_list_by_genre(genre)
         Genre.find_by_name(genre).movies.collect {|movie| movie.name}
     end
