be77620c8ff7710259479fb91c6ba6eb58bade35
Kenneth Spatta <kenyounot@gmail.com>
2019-10-03 23:16:52 -0400

    added list of summaries

--- lib/flixpick/scraper.rb
+++ lib/flixpick/scraper.rb
@@ -24,6 +24,7 @@ class Scraper
     def self.genre_list_scraper(urls)
         # hash of movie names with their tomato score, based on genre
         movie_genre_names = {}
+        @movie_genre_urls = {}
 
         # iterates through each genre and their url link to top 100 list
         urls.each do |k,v|
@@ -34,6 +35,7 @@ class Scraper
             movie_names = doc.css(".table").css('tr a')
             # array of genre scores
             movie_scores = doc.css(".table").css('.tMeterScore')
+        
                 
             # formats movie names; removes white space and junk characters
             movie_name_list = movie_names.collect {|movie| movie.text.strip}
@@ -45,11 +47,28 @@ class Scraper
                 movie_name_list[idx] + " // Tomato Score - #{m_score}%"
             end
 
+            movie_urls = movie_names.collect do |movie|
+                "https://rottentomatoes.com" + movie.attributes["href"].value
+            end
             # adds movie+score as one string to hash
             movie_genre_names[k] = movie_name_and_scores
+            @movie_genre_urls[k] = movie_urls
+            
         end
-
         movie_genre_names
+    end
+
+    def self.list_of_summary
+        movie_summaries = {}
+        @movie_genre_urls.each do |genre, url|
+            url.each do |link|
+                doc = Nokogiri::HTML(open(link))
+                summary = doc.css(".panel-body #movieSynopsis").text.strip 
+        
+                movie_summaries[genre] = summary
+            end
+        end
+        binding.pry
     end
 
     # returns list of genres
@@ -59,3 +78,8 @@ class Scraper
         genre_list
     end
 end
+
+Scraper.genre_list_scraper(Scraper.index_page_scraper("https://rottentomatoes.com/top"))
+Scraper.list_of_summary
+
+
