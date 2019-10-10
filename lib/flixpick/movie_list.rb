56c663a8e7e8878d33cbb5a66589715c656dbf90
Kenneth Spatta <kenyounot@gmail.com>
2019-10-10 13:53:28 -0400

    added some comments

--- lib/flixpick/movie_list.rb
+++ lib/flixpick/movie_list.rb
@@ -3,15 +3,16 @@ class Flixpick::MovieList
     # creates new instances of movies and generes and stores it in instance variable
     @movie_data = Movie.new_from_hash(Scraper.genre_list_scraper(Scraper.index_page_scraper("https://rottentomatoes.com/top")))
 
-
+    # list all of the genre names
     def self.list_of_genre_names
         Genre.all.collect {|g| g.name}
     end
-
+    # returns a random movie from list inputed
     def self.random_movie(input)
         create_movie_list_by_genre(input).sample
     end
 
+    # create a movie list by genre
     def self.create_movie_list_by_genre(genre)
         Genre.find_by_name(genre).movies.collect {|movie| movie.name}
     end
